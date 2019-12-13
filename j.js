ee7a8837dd2e9d32222743d365d051a3a0bae5d3
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2019-12-13 12:23:21 -0500

    got the correct syntax for accessing the information in the taunt array

--- j.js
+++ j.js
@@ -1,9 +1,10 @@
-
 $.ajax({
     url: "https://api.fungenerators.com/taunt/categories.json?start=0&limit=5",
     method: "GET"
 }).then(function(yoda) {
-    
-    console.log(yoda);
 
+    $(".insultdiv").text(JSON.stringify(yoda.contents[0][0].description))
+
+    console.log(yoda);
+    console.log(yoda.contents[0][0].description)
 })
