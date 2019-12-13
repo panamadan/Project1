b83cdae3ae32992cdafbc5d96f1606e1bbbb4770
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2019-12-13 11:12:54 -0500

    finally got the proper URL

--- j.js
+++ j.js
@@ -1,2 +1,9 @@
-const tauntURL = "https://api.fungenerators.com"
-//This API doesn't require an API key to be accessed
+
+$.ajax({
+    url: "https://api.fungenerators.com/taunt/categories.json?start=0&limit=5",
+    method: "GET"
+}).then(function(yoda) {
+    
+    console.log(yoda);
+
+})
